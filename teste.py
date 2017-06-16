b9dbf4c7d14715a71ad6cac17d1df085e200a13d
waacastro <waacastro@hotmail.com>
2017-06-16 09:23:34 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,18 @@
+import select
+import socket
+import sys
+import Queue
+
+array = []
+
+array = [1,"dois"]
+
+array.append('3')
+
+for i in array:
+	print i
+
+bla = ["a","b",1,"d","e","f"]
+
+print bla[4:]
+
